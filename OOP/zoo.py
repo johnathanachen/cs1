8dd1c96c19431205ca8f5d25e8116b9a7f89f211
Johnathan Chen <jc.swfit@icloud.com>
2017-09-27 16:23:47 -0700

    OOP 7

--- OOP/zoo.py
+++ OOP/zoo.py
@@ -1,7 +1,6 @@
 class Animal(object):
     def __init__(self, name):
         self.name = name
-        self.favoriteFood = favoriteFood
 
     def sleep(self):
         print(self.name , "sleeps for 8 hours")
@@ -48,12 +47,62 @@ class Bear(Animal):
         else:
             pass
 
-tiger = Tiger("Johnny")
-tiger.eat("meat")
-tiger.sleep()
+class Unicorn(Animal):
+    def __init__(self, name):
+        self.name = name
 
+    def sleep(self):
+        print(self.name, "sleeps in a cloud")
 
+    def eat(self, food):
+        self.food = food
+        favoriteFood = "marshmallows"
+        print(self.name,  "eats", food)
+        if food == favoriteFood:
+            print("YUM!", self.name,  "wants more", food)
+        else:
+            pass
 
-bear = Bear("Johnny")
-bear.eat("fish")
-bear.sleep()
+class Giraffe(Animal):
+    def __init__(self, name):
+        self.name = name
+
+    def sleep(self):
+        print(self.name, "sleeps for 8 hours")
+
+    def eat(self, food):
+        self.food = food
+        favoriteFood = "leaves"
+        print(self.name,  "eats", food)
+        if food == favoriteFood:
+            print("YUM!", self.name,  "wants more", food)
+        else:
+            print("YUCK!", self.name, "spits out", food)
+
+class Bee(Animal):
+    def __init__(self, name):
+        self.name = name
+
+    def sleep(self):
+        print(self.name, "never sleeps")
+
+    def eat(self, food):
+        self.food = food
+        favoriteFood = "pollen"
+        print(self.name,  "eats", food)
+        if food == favoriteFood:
+            print("YUM!", self.name,  "wants more", food)
+        else:
+            print("YUCK!", self.name, "spits out", food)
+
+class Zookeeper(object):
+    def __init__(self, name):
+        self.name = name
+
+    def feedAnimals(self, animals, food):
+        self.animals = animals
+        self.food = food
+        print(self.name, "is feeding", self.food, "to", len(self.animals), "animals")    
+        for i in animals:
+            i.eat(self.food)
+            i.sleep()
