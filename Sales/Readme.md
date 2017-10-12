7ac19cbd4bf244c1cd93ce3882bc087833f24cb4
Johnathan Chen <johnathanchenn@outlook.com>
2017-10-11 20:08:26 -0700

    highest sales in feb

---
+++ Sales/Readme.md
@@ -0,0 +1,20 @@
+1. What are the total amount of sales contained in this data set? 
+    - `$500,145,944.33`
+
+1. Which city had the highest sales in February? 
+    - `LA`
+
+1. Out of the entire data set, what is the total amount of money people have paid in cash?
+    - ``
+
+1. What is the most popular payment type in Oakland in March?
+     - ``
+
+1. How many sales were made on 4/20, and which city had the highest sales value?
+     - ``
+
+1. What is the average sales amount for credit card purchases?
+     - ``
+
+1. How many purchases were made by bartering with baseball cards?
+     - ``
---
+++ Sales/sales_questions.py
@@ -0,0 +1,59 @@
+import os
+
+with open('sales_data.txt', 'r') as dataSet:
+    arry_list = []
+    add_price_list = []
+    for word in dataSet:
+        arry_list.append(word.split())
+    
+def main():
+    # get_total_amount()
+    # highest_feb_city_sales()
+    cash_people()
+
+def get_total_amount():
+    for item_set in arry_list:
+        for single_word in item_set:
+            for digit in single_word:
+                if "$" in digit:
+                    price_list = list(single_word)
+                    price_list.remove("$")
+                    no_dollar_sign = ''.join(price_list)
+                    add_price_list.append(no_dollar_sign)
+    rounded_amount = round(sum(map(float, add_price_list)),2)
+    total_amount = '$' + str(rounded_amount)
+    print(total_amount)    
+
+def highest_feb_city_sales():
+    all_febs = []
+    all_sets_with_feb = []
+    all_feb_prices = []
+
+    #get all febs
+    for item_set in arry_list:
+        for single_word in item_set:
+            if "12/" in single_word:
+                pass
+            elif "2/" in single_word:
+                all_febs.append(single_word)
+                all_sets_with_feb.append(item_set)
+                # Making sure all index is price amount
+                if "$" in item_set[3]:
+                    all_feb_prices.append(item_set[3])
+                    max_price = max(all_feb_prices)
+                else: 
+                    pass
+            else:
+                pass
+    # Check which set's price amount match with max_price
+    for feb_set in all_sets_with_feb:
+        for feb_set_price in feb_set:
+            if feb_set_price == max_price:
+                best_location = feb_set[0]
+                print(best_location)
+
+    
+def cash_people():
+
+
+main()
